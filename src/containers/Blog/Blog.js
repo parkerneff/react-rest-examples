ab7d3bccbaabb3c52d9a0112492a366fdd8d069f
pneff <pneff@unicon.net>
2018-10-01 09:47:45 -0700

    151

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from "axios";
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                    this.setState({posts: response.data});
+                   //  console.log(response.data);
+                }
+            );
+    }
+
+    render() {
+
+        const posts = this.state.posts.map(post => {
+           // console.log("post title=" + post.title)
+            return <Post title={post.title} key={post.id} />
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
