293c19e7a25df46902c7b1373d45c9439dbe7b2b
pneff <pneff@unicon.net>
2018-10-03 09:12:29 -0700

    153

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from "axios";
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -28,11 +29,19 @@ class Blog extends Component {
             );
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render() {
 
         const posts = this.state.posts.map(post => {
             // console.log("post title=" + post.title)
-            return <Post title={post.title} key={post.id} author={post.author} />
+            return <Post
+                title={post.title}
+                key={post.id}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
         });
 
         return (
@@ -41,7 +50,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost/>
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost/>
