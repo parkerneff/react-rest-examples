efefc48379c981abcdae6151e5333c1c9e9636b3
pneff <pneff@unicon.net>
2018-10-03 11:22:58 -0700

    159 global request/response interceptors in axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.interceptors.request.use(request => {
+
+    console.log(request);
+    // Edit the request, i.e. add Auth headers
+    return request; // important. Will block request if not added
+
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+
+axios.interceptors.response.use(
+    response => {
+
+        console.log(response);
+        // Edit the request, i.e. add Auth headers
+        return response; // important. Will block request if not added
+
+    }, error => {
+        console.log(error);
+        return Promise.reject(error);
+    }
+
+);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
