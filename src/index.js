dd364a566b4d47388f8a5b0400cb873378fffd3e
pneff <pneff@unicon.net>
2018-10-04 14:32:50 -0700

    160 Axios default configuration

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common['Authorization'] = 'AUTHTOKEN';
+axios.defaults.headers.post['Content-Type'] = "application/json";  // is default, but shows how to override
 axios.interceptors.request.use(request => {
 
     console.log(request);
