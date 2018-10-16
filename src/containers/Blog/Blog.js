61fcd3d668bf90cbc2dde9a2711b1529b6467417
pneff <pneff@unicon.net>
2018-10-16 10:02:04 -0700

    203 dynamic imports (code splitting)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,17 +2,21 @@ import React, {Component} from 'react';
 
 
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
 import Posts from "./Posts/Posts";
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
-import NewPost from "./NewPost/NewPost";
+//import NewPost from "./NewPost/NewPost";
 import FullPost from "./FullPost/FullPost";
 //import axios from "axios";
 //import axios from '../../axios';
 
+const AsyncNewPost = asyncComponent(() => {
+    return import("./NewPost/NewPost");
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
 
@@ -35,7 +39,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/> : null}
                     <Route path="/posts"  component={Posts}/>
                     <Route render={() => <h1>Not Found</h1>}/>
                     /* <Redirect from="/" to="/posts/" /> */
