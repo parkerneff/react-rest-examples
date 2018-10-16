7cd1335b4871f46c6637f91d5ecece69737afd05
pneff <pneff@unicon.net>
2018-10-16 07:20:05 -0700

    198 nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -32,9 +32,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/" exact component={Posts}/>
                     <Route path="/new-post" component={NewPost}/>
-                    <Route path="/:id" exact component={FullPost}/>
+                    <Route path="/"  component={Posts}/>
+
                 </Switch>
 
 
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import axios from "../../../axios";
 import Post from "../../../components/Post/Post";
 import './Posts.css';
+import {Route} from "react-router-dom";
+import FullPost from "../FullPost/FullPost";
 
 class Posts extends Component {
 
@@ -41,20 +43,24 @@ class Posts extends Component {
             posts = this.state.posts.map(post => {
                 // console.log("post title=" + post.title)
                 return (
-                   // <Link to={"/" + post.id} key={post.id}>
+                    // <Link to={"/" + post.id} key={post.id}>
                     <Post
                         key={post.id}
                         title={post.title}
                         author={post.author}
                         clicked={() => this.postSelectedHandler(post.id)}/>)
-                  //  </Link>);
+                //  </Link>);
             });
         }
 
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path="/:id" exact component={FullPost}/>
+
+            </div>
         );
     }
 
